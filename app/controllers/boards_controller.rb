edb42acea272fca64da22f34971e725c0ca5afd0
Andrew Pham <69804999+andrewp8@users.noreply.github.com>
2024-02-24 05:24:42 +0000

    skip_action_before boards#index

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,5 @@
 class BoardsController < ApplicationController
+    skip_before_action(:authenticate_user!, {:only => [:index]})
   def index
     matching_boards = Board.all
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "photos#index"
+  root "boards#index"
   # get("/", { :controller => "boards", :action => "index" })
 
   # Routes for the Post resource:
